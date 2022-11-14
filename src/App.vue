cd695ef5fbb79bc84171aebe7f3d2878aac666c4
SaverioCascelli <saverio.cascelli@gmail.com>
2022-11-14 16:25:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+  <script>
+  
+  import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    name: "App",
+    components:{
+      AppTitle
+    }
+  }
+  </script>
+
+<template>
+  <AppTitle/>
+  <h1>h1 nell'app</h1>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+    <script>
+    export default {
+        name: "AppTitle"
+    }
+    </script>
+
+<template>
+  <h2>la mia prima app con Vite!</h2>
+</template>
+
+
+<style>
+    h2{
+        font-size: 2rem;
+        color: red;
+    }
+</style>
